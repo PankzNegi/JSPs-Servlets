80df02b76a221fac3a4b7397392fcb7fdc04f966
Pankz <pankznegi4638@gmail.com>
2023-03-11 19:20:01 +0530

    Creating and running First Servlet

---
+++ src/main/java/com/pankz/SimpleServlet.java
@@ -0,0 +1,31 @@
+package com.pankz;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class SimpleServlet
+ */
+public class SimpleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("Hello from GET method");
+		 PrintWriter writer=response.getWriter();
+		 
+		 writer.println("<html><body>");
+		 writer.println("Hello in HTML");
+		 writer.println("</body></html>");
+		
+	}
+
+}
