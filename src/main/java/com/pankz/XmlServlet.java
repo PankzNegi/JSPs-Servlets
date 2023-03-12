e77f0e518734ed57f7b14eeff4c6c2967c07fa4d
Pankz <pankznegi4638@gmail.com>
2023-03-12 21:21:17 +0530

    passing mnore parameters

--- src/main/java/com/pankz/XmlServlet.java
+++ src/main/java/com/pankz/XmlServlet.java
@@ -16,6 +16,7 @@ protected void doGet(HttpServletRequest request, HttpServletResponse response) t
 	response.setContentType("text/html");
 	PrintWriter writer=response.getWriter();
 	String userName=request.getParameter("userName");
+	
 	writer.println("Hello from GET method "+userName);
 }
 
@@ -24,6 +25,11 @@ protected void doPost(HttpServletRequest request, HttpServletResponse response) 
 	response.setContentType("text/html");
 	PrintWriter writer=response.getWriter();
 	String userName=request.getParameter("userName");
-	writer.println("Hello from POST method "+userName);
+	String fullName=request.getParameter("fullName");
+	writer.println("Hello from POST method "+userName+"! We know your fullname is "+fullName);
+	String prof=request.getParameter("prof");
+	writer.println("You are a " +prof);
+	String Location=request.getParameter("location");
+	writer.println("You are in "+Location);
 }
 }
