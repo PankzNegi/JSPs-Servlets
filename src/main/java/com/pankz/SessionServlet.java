0358465c849285267cc5183e95176b56bd9350dd
Pankz <pankznegi4638@gmail.com>
2023-03-14 19:02:17 +0530

    Configuring parameters in annotations/init()

--- src/main/java/com/pankz/SessionServlet.java
+++ src/main/java/com/pankz/SessionServlet.java
@@ -5,10 +5,14 @@ import java.io.PrintWriter;
 
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebInitParam;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+@WebServlet(description="SimpleServlet", urlPatterns= {"/SimpleServletPath"}
+, initParams= {@WebInitParam(name="defaultUser", value="Martin")})
 
 public class SessionServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
@@ -26,6 +30,7 @@ public class SessionServlet extends HttpServlet {
 		writer.println("Request Parameter has username as , " +userName);
 		writer.println("Session Parameter has username as , "+(String)session.getAttribute("saveduserName"));
 		writer.println("Context Parameter has username as , "+(String)context.getAttribute("saveduserName"));
+		writer.println("Init Parameter has default username as"+getServletConfig().getInitParameter("defaultUser"));
 	}
 
 }
